59b3aebc5d13b25cf4970888e9336fe698c05d6d
Andrei <adiaconu55@gmail.com>
2024-10-09 13:44:40 +0300

    Initial commit

---
+++ src/test/java/com/shop/shop/management/ShopManagementApplicationTests.java
@@ -0,0 +1,14 @@
+package com.shop.shop.management;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ShopManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertTrue(true);
+	}
+
+}
