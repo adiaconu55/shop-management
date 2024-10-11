715a9e02c00d7026a6f8e00f5124c1626b29f5a2
Andrei <adiaconu55@gmail.com>
2024-10-11 12:23:19 +0300

    Fixed file logging

--- src/main/java/com/shop/shop/management/domain/service/impl/ProductServiceImpl.java
+++ src/main/java/com/shop/shop/management/domain/service/impl/ProductServiceImpl.java
@@ -73,7 +73,7 @@ public class ProductServiceImpl implements ProductService {
     }
 
     public ProductDto getProduct(String productName){
-        log.warn("Fetching product {}", productName);
+        log.info("Fetching product {}", productName);
 
         Product product = productRepository.findByProductName(productName)
                 .orElseThrow(() -> {
